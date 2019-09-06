90510d291c759899288fad0a6d0ed3256c8105f4
Jerry Kurian <jerrykurian@gmail.com>
2019-09-06 07:00:15 +0530

    Initial Code

---
+++ src/main/groovy/com/sample/pipeline/PipelineApplication.groovy
@@ -0,0 +1,13 @@
+package com.sample.pipeline
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PipelineApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PipelineApplication, args)
+	}
+
+}
